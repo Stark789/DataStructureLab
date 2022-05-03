e084d5beef783211b90f4437707fd60f37d4d86a
Stark789 <85982161+Stark789@users.noreply.github.com>
2022-05-03 19:09:40 +0530

    Add files via upload

--- Binary search.cpp
+++ Binary search.cpp
@@ -1,36 +1,37 @@
 #include<iostream>
 using namespace std;
+int binarysearch(int arr[],int first,int last,int search);
 int main()
 {
-	int n,i,arr[10],s,first=0,middle,last;
-	cout<<"Enter the number of array:"<<endl;
+	int n,i,search,loc;
+	cout<<"Enter the number of elements:"<<endl<<"\t";
 	cin>>n;
-	cout<<"Enter the elements of the array:"<<endl;
+	int arr[n];
+	int first=0,last;
+	cout<<"Enter the elements:"<<endl;
 	for(i=0;i<n;i++)
 	cin>>arr[i];
-	cout<<endl;
-	cout<<"the elements that you entered are as follows:"<<endl;
+	cout<<"The elements that you entered:"<<endl;
 	for(i=0;i<n;i++)
 	cout<<arr[i]<<endl;
-	cout<<"Enter the element that you need to be searched: \t";
-	cin>>s;
-	cout<<s<<endl;
+	cout<<"Enter the element that your lokking for:"<<endl;
+	cin>>search;
 	last=n-1;
-	middle=(first+last)/2;
-	while(first<=last)
-	{
-		if(s<arr[middle])
-		last=middle-1;
-		else if(s==arr[middle])
-		{
-			cout<<"the searched element:"<<s<<"is found at: "<<middle+1;
-			break;
-		}
-		else
-		first=middle+1;
-		middle=(first+last)/2;
-	}
-	if(first>last)
-	cout<<"the searched element :"<<s<<"is not found in the given array of elements"<<endl;
+	loc=binarysearch(arr,first,last,search);
+	cout<<"the element that you wanted to search for is:"<<search<<"  is found at the positon of the array: \t"<<loc;
 	return 0;
+}
+int binarysearch(int arr[],int first,int last,int search)
+{
+	int middle;
+	if(first<=last)
+	{
+		middle=(first+last)/2;
+		if(arr[middle]==search)
+			return middle+1;
+		else if(arr[middle]<=search)
+			return binarysearch(arr,middle+1,last,search);
+		else
+			return binarysearch(arr,first,middle-1,search);
+	}
 }
